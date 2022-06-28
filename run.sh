3320de77b9b03e14e2521190941997f5431650de
Wojtek Hołowiak <w.holowiak@gmail.com>
2022-06-28 21:19:16 +0200

    Created a docker image for frontend and incorporated it into docker-compose file.

--- run.sh
+++ run.sh
@@ -9,6 +9,11 @@ mvn clean package -DskipTests
 docker build -t car_maintenance_tracker_api .
 cd ..
 
+#Build frontend
+cd CMT\ Frontend/
+docker build -t car_maintenance_tracker_frontend .
+cd ..
+
 #Run the application
 cd docker
 docker compose up -d
