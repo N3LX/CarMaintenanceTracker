8377812ae67c86f08144523e9a318d0ca9bafcbd
Wojtek Hołowiak <w.holowiak@gmail.com>
2022-06-28 18:46:09 +0200

    Added logging to ApiException class.

--- CMT API/src/main/java/com/n3lx/exception/ApiException.java
+++ CMT API/src/main/java/com/n3lx/exception/ApiException.java
@@ -1,5 +1,8 @@
 package com.n3lx.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -8,12 +11,18 @@ import java.time.format.DateTimeFormatter;
  */
 public class ApiException {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiException.class);
+
     private String message;
     private String timestamp;
 
     public ApiException(String message) {
         this.message = message;
         this.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        //Log this incident
+        LOGGER.debug("An API exception has been thrown with following details:\n" +
+                "Message: " + message);
     }
 
     public String getMessage() {
